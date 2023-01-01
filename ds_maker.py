26eeac73dfb34cc5dcea750d8cb73e9bc35db6f7
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-01-01 15:51:20 +1100

    xy

--- ds_maker.py
+++ ds_maker.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 x = np.random.uniform(0, 1, (1000,3))
-x[:,2] = x[:,0] * x[:,1]
+x[:,2] = (x[:,0]**2) + (2 * x[:,0] * x[:,1]) + (x[:,1]**2)
 
 columns = ["x1", "x2", "y"]
 df = pd.DataFrame(data=x, columns=columns)
